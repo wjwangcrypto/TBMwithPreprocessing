a7b151871c3c80d932b42370686a1363c66d1976
Weijia Wang <52818338+wjwangcrypto@users.noreply.github.com>
2024-04-25 22:18:26 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,1 @@
+This code package contains the AES implementation accompanying the paper "Efficient Table-Based Masking with Pre-processing".
---
+++ main.c
@@ -0,0 +1,34 @@
+
+extern char AES_one_round_pre();
+extern char AES_one_round_online();
+
+int main(void)
+{
+	// AES round function. 
+	
+	AES_one_round_pre(); 
+	AES_one_round_online();
+	
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
